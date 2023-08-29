ab7f6132618d88232ab579f1700b906b0652f512
GianmarcoBramucci <giangian797@gmail.com>
2023-08-29 23:58:20 +0200

    add script

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const team=
+    [
+       {
+        nome:'Wayne Barnett',
+        posizione:'Founder & CEO',
+        img:'wayne-barnett-founder-ceo.jpg'
+       },
+       {					
+        nome:'Angela Caroll',
+        posizione:'Chief Editor',
+        img:'angela-caroll-chief-editor.jpg'
+       },
+       {
+        nome:'Walter Gordon',
+        posizione:'Office Manager',
+        img:'walter-gordon-office-manager.jpg'
+       },
+       {
+        nome:'Angela Lopez',
+        posizione:'Social Media Manager',
+        img:'angela-lopez-social-media-manager.jpg'
+       },
+       {
+        nome:'Scott Estrada',
+        posizione:'Developer',
+        img:'scott-estrada-developer.jpg'
+       },
+       {
+        nome:'Barbara Ramos',
+        posizione:'Graphic Designer',
+        img:'barbara-ramos-graphic-designer.jpg'
+       }
+    ];
+    for(let i=0;i<team.length;i++){
+        console.log(team[i]);
+        for(let j in team){
+            
+        };
+    };
+    
+			
+						
+				
